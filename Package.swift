902c24390b55fa93f5d93337fa7465d1f8da3d73
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-05-08 14:35:58 +0200

    using Tasks

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
     	.Package(url: "https://github.com/VeniceX/HTTPSClient.git", majorVersion: 0, minor: 7),
     	.Package(url: "https://github.com/Zewo/XML.git", majorVersion: 0, minor: 7),
     	.Package(url: "https://github.com/czechboy0/Redbird.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/czechboy0/Jay.git", majorVersion: 0, minor: 6)
+        .Package(url: "https://github.com/czechboy0/Jay.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/czechboy0/Tasks.git", majorVersion: 0, minor: 1)
     ]
 )
--- Sources/PackageExporter/PackageParser.swift
+++ Sources/PackageExporter/PackageParser.swift
@@ -1,5 +1,7 @@
 
 func parsePackage(path: String) throws {
-    //TODO: waiting for https://github.com/apple/swift-package-manager/pull/318
+
+    
+
 }
 
