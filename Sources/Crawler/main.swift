f546c46c3c41bd91a29f65362e60a8e17ce0323e
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-05-10 09:02:19 +0200

    adding flags

--- Sources/Crawler/main.swift
+++ Sources/Crawler/main.swift
@@ -7,19 +7,42 @@
 //
 
 import Redbird
+import Utils
+
+var args = Process.arguments.dropFirst()
+
+enum Flag: String {
+    case noSearch = "--no-search"
+}
 
 do {
+    
+    var flag: Flag? = nil
+    if let flagString = args.popFirst() {
+        if let parsedFlag = Flag(rawValue: flagString) {
+            flag = parsedFlag
+        } else {
+            throw Error("Unrecognized flag '\(flagString)'")
+        }
+    }
+    
+    var skipSearch = flag == Flag.noSearch
+    var skipFileFetch = false
     
     print("Crawler starting")
     
     //you need to have a redis-server running at 127.0.0.1 port 6379
     let db = try Redbird()
     
-    //fetches repo names
-    try SearchResultsFetcher().crawlRepoNames(db: db)
+    if !skipSearch {
+        //fetches repo names
+        try SearchResultsFetcher().crawlRepoNames(db: db)
+    }
     
-    //download Package.swift for each package
-    try PackageFileFetcher().crawlRepoPackageFiles(db: db)
+    if !skipFileFetch {
+        //download Package.swift for each package
+        try PackageFileFetcher().crawlRepoPackageFiles(db: db)
+    }
     
     print("Crawler finished")
     
