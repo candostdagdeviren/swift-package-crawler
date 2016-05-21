2fb2a1ca868b91daa556f6f2832fdbc6b38bbf78
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-05-21 16:42:09 +0100

    merge

--- Sources/Server/docs.swift
+++ Sources/Server/docs.swift
@@ -7,11 +7,15 @@
  - endpoint for getting a project's Package.swift: /packages/github/USER/NAME/manifest
  - endpoint for getting a project's Package.swift in JSON format: /packages/github/USER/NAME/manifest/json
  
- Stats:
+ Package Info:
  - endpoint for getting project's direct dependencies /packages/github/USER/NAME/stats/dependencies/direct
  - endpoint for getting project's transitive dependencies /packages/github/USER/NAME/stats/dependencies/transitive
- - endpoint for getting user's direct dependencies /packages/github/USER/stats/dependencies/direct
- - endpoint for getting user's transitive dependencies /packages/github/USER/stats/dependencies/transitive
+ 
+ Stats:
+ - endpoint for getting project's direct dependees /packages/github/USER/NAME/stats/dependees/direct
+ - endpoint for getting project's transitive dependees /packages/github/USER/NAME/stats/dependees/transitive
+ - endpoint for getting user's direct dependees /packages/github/USER/stats/dependees/direct
+ - endpoint for getting user's transitive dependees /packages/github/USER/stats/dependees/transitive
  
  Data queries:
  - endpoint for getting the list of GitHub project names /data/github/names (paging API)
